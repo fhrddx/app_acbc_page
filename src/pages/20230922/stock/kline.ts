0d1cebbeaffec18e77dd5afbaee7cfefc37a8c1c
vincent <1339301794@qq.com>
2025-09-08 01:53:44 +0800

    1

--- src/pages/20230922/stock/kline.ts
+++ src/pages/20230922/stock/kline.ts
@@ -9,14 +9,22 @@ export default class kline{
   private code: string;
   private container: any;
   private canvas: any;
+  private width: number;
+  private height: number;
+  private area1: number;
+  private area2: number;
+  private area3: number;
 
   constructor(params: klineParams){
     this.code = params.code;
     this.container = params.container;
     this.canvas = params.canvas;
-    
-    alert(this.container.clientHeight);
-
+    const { clientHeight, clientWidth } = this.container;
+    this.width = clientWidth;
+    this.height = clientHeight;
+    this.area3 = clientHeight * 0.3;
+    this.area2 = clientHeight * 0.1;
+    this.area1 = clientHeight * 0.6;
   }
 
   draw(){
